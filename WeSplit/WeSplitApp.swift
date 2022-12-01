84c5a993e802dad6f39c1cd37015114871628a5d
Aaron <46139215+Razzolate99@users.noreply.github.com>
2022-12-01 01:05:36 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aaron Ward on 2022-12-01.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
